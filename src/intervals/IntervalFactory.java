d3c12984c0b6a2911e758b839b6b5de5fdcca905
Leire <Leire@Rei>
2015-03-29 11:40:35 +0200

    Creado el metido getInterval

--- src/intervals/IntervalFactory.java
+++ src/intervals/IntervalFactory.java
@@ -1,9 +1,20 @@
 package intervals;
 
-
 public class IntervalFactory {
 
-	public static Interval getInterval(double minimum, double maximum) {
-		return new Interval(minimum, maximum);
+	public static Interval getInterval(double minimum, double maximum,
+			Opening opening) {
+		switch (opening) {
+		case BOTH_OPENED:
+			return new BothOpened(minimum, maximum);
+		case LEFT_OPENED:
+			return new LeftOpened(minimum, maximum);
+		case RIGHT_OPENED:
+			return new RightOpened(minimum, maximum);
+		case UNOPENED:
+			return new UnOpened(minimum, maximum);
+		default:
+			throw new IllegalArgumentException("Opening Incorrecto");
+		}
 	}
 }
