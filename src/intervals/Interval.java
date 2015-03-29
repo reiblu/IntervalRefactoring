85ff8c5cf5c063e90389d323a9c5716662d1e987
Leire <Leire@Rei>
2015-03-29 11:42:31 +0200

    Cambiados los metodos de interval a abstractos

--- src/intervals/Interval.java
+++ src/intervals/Interval.java
@@ -2,11 +2,9 @@ package intervals;
 
 public abstract class Interval {
 
-    private double minimum;
+    protected double minimum;
 
-    private double maximum;
-
-    private Opening opening;
+    protected double maximum;
 
     public Interval(double minimum, double maximum) {
         this.minimum = minimum;
@@ -21,46 +19,11 @@ public abstract class Interval {
         return (minimum + maximum) / 2;
     }
 
-    public boolean includes(double value) {
-        boolean resultado = false;
-        if (minimum < value && value < maximum) {
-            return resultado = true;
-        } else if (minimum == value) {
-            if (opening == Opening.RIGHT_OPENED || opening == Opening.UNOPENED) {
-                return resultado = true;
-            }
-        } else if (maximum == value) {
-            if (opening == Opening.LEFT_OPENED || opening == Opening.UNOPENED) {
-                return resultado = true;
-            }
-        }
-        return resultado;
-    }
+    public abstract boolean includes(double value);
+     
 
-    public boolean includes(Interval interval) {
-        boolean resultado = false;
-        if (includes(interval.minimum) && includes(interval.maximum)) {
-            resultado = true;
-        } else if ((minimum == interval.minimum) && this.maximum > interval.maximum) {
-            if (opening == Opening.UNOPENED || opening == Opening.RIGHT_OPENED) {
-                resultado = true;
-            } else if ((opening == Opening.BOTH_OPENED) || (opening == Opening.LEFT_OPENED)) {
-                if((interval.opening == Opening.BOTH_OPENED) || (interval.opening == Opening.LEFT_OPENED)){
-                    resultado = true;
-                }
-            }
-
-        } else if ((maximum == interval.maximum)) {
-            if (opening == Opening.UNOPENED || opening == Opening.LEFT_OPENED) {
-                resultado = true;
-            } else if ((opening == Opening.BOTH_OPENED) || (opening == Opening.RIGHT_OPENED)) {
-                if((interval.opening == Opening.BOTH_OPENED) || (interval.opening == Opening.RIGHT_OPENED)){
-                    resultado = true;
-                }
-            }
-        }
-        return resultado;
-    }
+    public abstract boolean includes(Interval interval);
+       
 
     public boolean intersectsWith(Interval interval) {
         // TODO
